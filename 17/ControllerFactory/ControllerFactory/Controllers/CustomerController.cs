1de178b494add992f9204b545af23a47ea028019
t430 <evaseemefly@126.com>
2016-10-08 20:16:58 +0800

    10月8日最终

---
+++ 17/ControllerFactory/ControllerFactory/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ControllerFactory.Models;
+
+namespace ControllerFactory.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View("Result", new Result
+            {
+                ActionName = "Index",
+                ControllerName = "Customer"
+            });
+        }
+
+        public ViewResult List()
+        {
+            return View("Result", new Result
+            {
+                ActionName = "List",
+                ControllerName = "Customer"
+            });
+        }
+    }
+}
---
+++ 17/ControllerFactory/ControllerFactory/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ControllerFactory.Models;
+
+namespace ControllerFactory.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View("Result", new Result
+            {
+                ActionName = "Index",
+                ControllerName = "Product"
+            });
+        }
+
+        public ViewResult List()
+        {
+            return View("Result", new Result
+            {
+                ActionName = "List",
+                ControllerName = "Product"
+            });
+        }
+    }
+}
