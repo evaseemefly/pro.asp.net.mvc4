f993db55e7a3031c8b642d2f5a1c14dc72c3f096
t430 <evaseemefly@126.com>
2016-10-08 14:16:06 +0800

    10月8日

---
+++ 15/WebApplication1/WebApplication1/Controllers/RedirectController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class RedirectController : Controller
+    {
+        // GET: Redirect
+        public ActionResult RedirectToRoute()
+        {
+            TempData["Message"] = "sms";
+            TempData["Date"] = DateTime.Now;
+            return RedirectToAction("Index");
+        }
+
+         public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
