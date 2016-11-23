43d5317bc9894f1e54cf4a8e5b304accec25282a
liusihan-单位 <evaseemefly@126.com>
2016-11-23 17:14:50 +0800

    添加对于异步请求的测试

---
+++ 17/ControllerFactory/ControllerFactory/Controllers/RemoteDataController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+namespace ControllerFactory.Controllers
+{
+    public class RemoteDataController : AsyncController
+    {
+        // GET: RemoteData
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Data()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> DataAsync()
+        {
+
+            string data = await new ControllerFactory.Service.RemoteService().GetRemoteDataAsync();
+            return View("Data", (object)data);
+        }
+
+       
+    }
+}
---
+++ 17/ControllerFactory/ControllerFactory/Service/RemoteService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ControllerFactory.Service
+{
+    public class RemoteService
+    {
+        public string GetRemoteData()
+        {
+            Thread.Sleep(2000);
+            return "hello world";
+        }
+
+        public async Task<string> GetRemoteDataAsync()
+        {
+            return await Task<string>.Factory.StartNew(() =>
+            {
+                Thread.Sleep(10000);
+                return "hello world";
+            });
+        }
+    }
+}
